cd9d3d5247976dd8596a04771403cb05891a508a
EddyGharbi <eddygharbiema@gmail.com>
2020-04-22 13:12:46 -0700

    Fix wrong install working dir

--- mac/install-plugin.sh
+++ mac/install-plugin.sh
@@ -1,6 +1,6 @@
-rm -rf vcam-plugin.plugin
-mkdir vcam-plugin.plugin
-cd vcam-plugin.plugin
+rm -rf $1/vcam-plugin.plugin
+mkdir $1/vcam-plugin.plugin
+cd $1/vcam-plugin.plugin
 
 mkdir Contents
 cd Contents
@@ -10,12 +10,14 @@ mkdir Resources
 cd ..
 cd ..
 
-cp ./data/obs-plugins/slobs-virtual-cam/libvcam-plugin.dylib ./vcam-plugin.plugin/Contents/MacOS/vcam-plugin
-cp ./data/obs-plugins/slobs-virtual-cam/vcam-assistant ./vcam-plugin.plugin/Contents/Resources/vcam-assistant
+echo $1
 
-cp ./data/obs-plugins/slobs-virtual-cam/Info.plist ./vcam-plugin.plugin/Contents/Info.plist
+cp $1/libvcam-plugin.dylib ./vcam-plugin.plugin/Contents/MacOS/vcam-plugin
+cp $1/vcam-assistant ./vcam-plugin.plugin/Contents/Resources/vcam-assistant
+
+cp $1/Info.plist ./vcam-plugin.plugin/Contents/Info.plist
 
 rm -rf /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
-cp -a ./vcam-plugin.plugin/. /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
+cp -a $1/vcam-plugin.plugin/. /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
 
-rm -rf vcam-plugin.plugin
+rm -rf $1/vcam-plugin.plugin
