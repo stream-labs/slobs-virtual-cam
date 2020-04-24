f81b60531471f17b114508503222d06b40611b52
EddyGharbi <eddygharbiema@gmail.com>
2020-04-24 16:13:36 -0700

    Create temp files in temp directory

--- mac/install-plugin.sh
+++ mac/install-plugin.sh
@@ -1,6 +1,6 @@
-rm -rf $1/vcam-plugin.plugin
-mkdir $1/vcam-plugin.plugin
-cd $1/vcam-plugin.plugin
+rm -rf /tmp//vcam-plugin.plugin
+mkdir /tmp/vcam-plugin.plugin
+cd /tmp/vcam-plugin.plugin
 
 mkdir Contents
 cd Contents
@@ -10,12 +10,12 @@ mkdir Resources
 cd ..
 cd ..
 
-cp $1/libvcam-plugin.dylib ./vcam-plugin.plugin/Contents/MacOS/vcam-plugin
-cp $1/vcam-assistant ./vcam-plugin.plugin/Contents/Resources/vcam-assistant
+cp $1/libvcam-plugin.dylib /tmp/vcam-plugin.plugin/Contents/MacOS/vcam-plugin
+cp $1/vcam-assistant /tmp/vcam-plugin.plugin/Contents/Resources/vcam-assistant
 
-cp $1/Info.plist ./vcam-plugin.plugin/Contents/Info.plist
+cp $1/Info.plist /tmp/vcam-plugin.plugin/Contents/Info.plist
 
 rm -rf /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
-cp -a $1/vcam-plugin.plugin/. /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
+cp -a /tmp/vcam-plugin.plugin/. /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
 
-rm -rf $1/vcam-plugin.plugin
+rm -rf /tmp/vcam-plugin.plugin
