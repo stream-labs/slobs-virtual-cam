ac4069ebf0fbba9ad61a9c721a0892040f2236dd
EddyGharbi <eddygharbiema@gmail.com>
2020-04-28 12:08:35 -0700

    Fix wrong path in install-script

--- mac/install-plugin.sh
+++ mac/install-plugin.sh
@@ -10,10 +10,10 @@ mkdir Resources
 cd ..
 cd ..
 
-cp $1/libvcam-plugin.dylib /tmp/vcam-plugin.plugin/Contents/MacOS/vcam-plugin
-cp $1/vcam-assistant /tmp/vcam-plugin.plugin/Contents/Resources/vcam-assistant
+cp "$1/libvcam-plugin.dylib" /tmp/vcam-plugin.plugin/Contents/MacOS/vcam-plugin
+cp "$1/vcam-assistant" /tmp/vcam-plugin.plugin/Contents/Resources/vcam-assistant
 
-cp $1/Info.plist /tmp/vcam-plugin.plugin/Contents/Info.plist
+cp "$1/Info.plist" /tmp/vcam-plugin.plugin/Contents/Info.plist
 
 rm -rf /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
 cp -a /tmp/vcam-plugin.plugin/. /Library/CoreMediaIO/Plug-Ins/DAL/vcam-plugin.plugin
